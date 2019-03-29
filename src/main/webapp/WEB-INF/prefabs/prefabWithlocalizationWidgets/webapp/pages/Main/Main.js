688aa25a5271be57e54e07fa65b56c68ed15d4cf
Tejaswi Maryala <tejaswi.maryala@wavemaker.com>
2019-03-29 06:28:33 +0000

    Initial Push from 10.0.0_migration

--- src/main/webapp/WEB-INF/prefabs/prefabWithlocalizationWidgets/webapp/pages/Main/Main.js
+++ src/main/webapp/WEB-INF/prefabs/prefabWithlocalizationWidgets/webapp/pages/Main/Main.js
@@ -1,14 +1,12 @@
-Application.$controller("PrefabWithlocalizationWidgetsController",["$scope", function ($scope) {
-	"use strict";
+/*
+ * This function will be invoked when any of this prefab's property is changed
+ * @key: property name
+ * @newVal: new value of the property
+ * @oldVal: old value of the property
+ */
 
-	/*
-	 * This function will be invoked when any of this prefab's property is changed
-	 * @key: property name
-	 * @newVal: new value of the property
-	 * @oldVal: old value of the property
-	 */
-	function propertyChangeHandler(key, newVal, oldVal) {
-		/*
+function propertyChangeHandler(key, newVal, oldVal) {
+    /*
 		switch (key) {
 			case "prop1":
 				// do something with newVal for property 'prop1'
@@ -18,11 +16,12 @@ Application.$controller("PrefabWithlocalizationWidgetsController",["$scope", fun
 				break;
 		}
 		*/
-	}
-	/* register the property change handler */
-	$scope.propertyManager.add($scope.propertyManager.ACTIONS.CHANGE, propertyChangeHandler);
 
-	$scope.onInitPrefab = function() {
-		// this method will be triggered post initialization of the prefab.
-	};
-}]);
+}
+/* register the property change handler */
+
+Prefab.onPropertyChange = propertyChangeHandler;
+Prefab.onReady = function () {
+    // this method will be triggered post initialization of the prefab.
+};
+
