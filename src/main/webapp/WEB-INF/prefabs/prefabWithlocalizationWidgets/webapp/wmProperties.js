0e898afb23b5202a372ed77e8612aee53ba63e47
Tejaswi Maryala <tejaswi.maryala@wavemaker.com>
2019-03-29 06:28:05 +0000

    Initial Push

---
+++ src/main/webapp/WEB-INF/prefabs/prefabWithlocalizationWidgets/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "prefabWithlocalizationWidgets",
+  "homePage" : "Main",
+  "name" : "prefabWithlocalizationWidgets",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en,te",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.04"
+};
